6d73c8ff3a0270acc3e0fbf9a8a110eb9e52cc73
MrBearing <takumi1988okamoto@gmail.com>
2026-02-11 02:11:08 +0000

    記述追加

--- mikata_arm_moveit/launch/spawn_controllers.launch.py
+++ mikata_arm_moveit/launch/spawn_controllers.launch.py
@@ -1,7 +1,38 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
 from moveit_configs_utils import MoveItConfigsBuilder
-from moveit_configs_utils.launches import generate_spawn_controllers_launch
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("mikata_arm", package_name="mikata_arm_moveit").to_moveit_configs()
-    return generate_spawn_controllers_launch(moveit_config)
+    moveit_config = MoveItConfigsBuilder(
+        "mikata_arm", package_name="mikata_arm_moveit"
+    ).to_moveit_configs()
+
+    controller_names = moveit_config.trajectory_execution.get(
+        "moveit_simple_controller_manager", {}
+    ).get("controller_names", [])
+
+    # `ros2_control_node` が立ち上がる前に spawner が service call を始めると
+    # `list_controllers` が timeout して落ちることがあるため、待ち時間を長めに取る。
+    spawner_common_args = [
+        "-c",
+        "/controller_manager",
+        "--controller-manager-timeout",
+        "60",
+        "--service-call-timeout",
+        "30",
+    ]
+
+    nodes = []
+    for controller in controller_names + ["joint_state_broadcaster"]:
+        nodes.append(
+            Node(
+                package="controller_manager",
+                executable="spawner",
+                arguments=[controller, *spawner_common_args],
+                output="screen",
+            )
+        )
+
+    return LaunchDescription(nodes)
